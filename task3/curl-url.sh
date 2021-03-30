221a0d0cb29ed57a3d8b275df556e56eeb5fe8b0
alihussainia <alikasbati@outlook.com>
2021-03-31 04:14:27 +0500

    fetched port

--- task3/curl-url.sh
+++ task3/curl-url.sh
@@ -1,5 +1,7 @@
 for ((i=1;i<=100;i++)); 
-do
-    curl -s "http://localhost" | grep "<title>.*</title>"
+do  
+
+    VAR=$(kubectl get svc nginx-service | grep -i ":[0-9]*[^/]" | cut -d " " -f15 | cut -b 4-8)
+    curl -s "http://localhost:$VAR" | grep "<title>.*</title>"
     sleep 2s
 done
--- task3/curl2.sh
+++
@@ -1,2 +0,0 @@
-VAR=$(kubectl get svc nginx-service | grep -i ":[0-9]*[^/]" | cut -d " " -f15 | cut -b 4-8)
-   curl -s "http://localhost:$VAR" | grep "<title>.*</title>"
