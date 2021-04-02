f8d839e8c3403ac4007b04578d2a4ba7ef85370a
MUHAMMAD ALI <malirashid1994@gmail.com>
2021-04-02 15:05:10 +0500

    Updated the shell script

--- rollout-rollback/curl.sh
+++ rollout-rollback/curl.sh
@@ -1,8 +1,6 @@
 for ((i=1;i<=100;i++)); 
 do  
 
-    #VAR=$(kubectl get svc nginx-service | grep -i ":[0-9]*[^/]" | cut -d " " -f15 | cut -b 4-8)
-    #curl -s "http://localhost:$VAR" | grep "<title>.*</title>"
     curl -s "http://localhost" | grep "<title>.*</title>"
     sleep 2s
 done
